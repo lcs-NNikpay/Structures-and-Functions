1f9640b2d5ce0403aaea99e5dcb6e6e93c80b19f
Nicki Nikpay <nnikpay@lcs-mba3214.lcs.on.ca>
2017-12-04 15:00:36 -0500

    Done!

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -42,9 +42,6 @@ struct Point {
 
 }
 
-// Create an instance of the point from our problem
-var Cabinsite = Point(x: 6.0, y: 1.5)
-
 // Define a structure that represents slope
 struct Slope {
     var rise : Double = 1.0
@@ -58,11 +55,6 @@ struct Line {
     
 }
 
-
-// Create an instance of the line from our problem
-var SlopeOfExsistingRoad = Slope (rise : -1 , run : 2)
-var existingRoad = Line( slope: SlopeOfExsistingRoad, verticalIntercept: 9.5)
-
 ///  Returns the slope of line perpendicular to the provided line
 ///
 /// - Parameter givenLine: The line that we are starting from
@@ -72,8 +64,6 @@ func getSlopeOfPerpendicularLine(from givenLine: Line) -> Slope {
     return Slope(rise: givenLine.slope.run, run: givenLine.slope.rise * -1)
 }
 
-// get the perpendicular slope ( the slope of the new road from the cabin to the existing road )// defining the function
-let perpendicularSlope = getSlopeOfPerpendicularLine(from: existingRoad)
 func getverticalIntercept( from p : Point, onLinewith m : Slope) -> Double {
     
     // get the slope as a decimal
@@ -82,11 +72,6 @@ func getverticalIntercept( from p : Point, onLinewith m : Slope) -> Double {
     // b = y - m * x
     return p.y - mAsdecimal * p.x
 }
-// Invoking the function
- let perpendicularlineVerticalIntercept = getverticalIntercept(from: Cabinsite, onLinewith: perpendicularSlope)
-
-// Define the equation of the new line ( the new road that needs to be built )
-let newRoad = Line(slope: perpendicularSlope, verticalIntercept: perpendicularlineVerticalIntercept)
 
 /// Gets the point where two lines in a Cartesian plane intersect
 ///
@@ -102,9 +87,9 @@ func getpointOfInterception(between first : Line, and second : Line ) -> Point {
     let verticalInterceptDifference = first.verticalIntercept - second.verticalIntercept
     
     // Get the slope difference
-    let slopeDifference = second.slope.rise / second.slope.run - first.slope.rise / second.slope.run
+    let slopeDifference = second.slope.rise / second.slope.run - first.slope.rise / first.slope.run
     
-// Get the actual x-value (abscissa)
+    // Get the actual x-value (abscissa)
     let x = verticalInterceptDifference / slopeDifference
     
     // Get the actual y-value (subtitution) (ordinate)
@@ -116,6 +101,53 @@ func getpointOfInterception(between first : Line, and second : Line ) -> Point {
     return Point(x: x, y: y)
 }
 
-// Invoke the function to get the point of intersection
-getpointOfInterception(between: existingRoad, and: newRoad)
+///  Finds the distance between two points
+///
+/// - Parameters:
+///   - from: the first point
+///   - to: the second point
+/// - Returns: the distance
+func distance(from: Point, to: Point) -> Double {
+    // Return the distance (from Pythagorean's Theorem)
+    return sqrt( pow(from.x - to.x, 2) + pow(to.y - from.y, 2)  )
+    
+}
+
+/// Finds the shortest distance from a given point to a given line. (This is the distance formed from a perpendicular line segment.)
+///
+/// - Parameters:
+///   - providedPoint: a point on a Cartesian plane
+///   - providedLine: a line
+/// - Returns: the shortest distance
+func shortestDistance(from providedPoint: Point, to providedLine : Line ) -> Double {
+    
+    // Get the perpendicular slope (the slope of new road from the cabin to existing road)
+    let perpendicularSlope = getSlopeOfPerpendicularLine(from: providedLine)
+    
+    // Get the vertical intercept of the new road
+    let perpendicularLineVerticalIntercept = getverticalIntercept(from: providedPoint, onLinewith: perpendicularSlope)
+   
+    // Define the equation of the new line (the new road that needs to be built)
+    let newLine = Line(slope: perpendicularSlope, verticalIntercept: perpendicularLineVerticalIntercept)
+    
+    // Invoke the function to get the point of intersection
+    let pointOfIntersection = getpointOfInterception(between: providedLine, and: newLine)
+    
+    // Now actually find the shortest distance
+    let shortestDistance = distance(from: providedPoint, to: pointOfIntersection)
+    
+    // Give back the shortest distance
+    return shortestDistance
+    
+}
+
+// Create an instance of the point from our problem
+var cabinSite = Point(x: 6, y: 1.5)
+
+// Create an instance of the line from our problem
+var slopeOfExistingRoad = Slope(rise: -1, run: 2)
+var existingRoad = Line(slope: slopeOfExistingRoad, verticalIntercept: 9.5)
+
+// Use the overall function
+shortestDistance(from: cabinSite, to: existingRoad)
 
